27f1ed1637437ea7b0b98cb3005eb93a215f11e1
duccong2810 <ndcongts@gmail.com>
2022-11-12 16:25:19 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), preact()]
 });
